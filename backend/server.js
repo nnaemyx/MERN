91495dc29121b2c6ddd7c1d867b1182140058a11
tezon <tezonteam@gmail.com>
2022-07-22 16:23:46 -0700

    new

--- backend/server.js
+++ backend/server.js
@@ -18,9 +18,10 @@ if (process.env.NODE_ENV === 'production') {
         path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
       )
     );
-  } else {
+ 
+}else {
     app.get('/', (req, res) => res.send('Please set to production'));
-  }
+} 
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
