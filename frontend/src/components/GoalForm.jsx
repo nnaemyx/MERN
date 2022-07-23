3f338a4831f6a437304593a4a131c9cb798014ba
tezon <tezonteam@gmail.com>
2022-07-23 00:58:58 -0700

    new

--- frontend/src/components/GoalForm.jsx
+++ frontend/src/components/GoalForm.jsx
@@ -2,7 +2,7 @@ import {useState} from 'react'
 import {useDispatch} from 'react-redux'
 import {createGoal} from '../features/goals/goalSlice'
 function GoalForm() {
-    const [text, setText] = useState('')
+    const [text, setText] = useState([])
 
     const dispatch = useDispatch()
 
