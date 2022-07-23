363f54303e67a601ecc77742ec9d12a7448c60e8
tezon <tezonteam@gmail.com>
2022-07-23 01:25:08 -0700

    new

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -4,7 +4,8 @@ import { useSelector, useDispatch } from 'react-redux'
 import GoalForm from '../components/GoalForm'
 import GoalItem from '../components/GoalItem'
 import Spinner from '../components/Spinner'
-import { getGoals, reset } from '../features/goals/goalSlice'
+import { getGoals } from '../features/goals/goalSlice'
+import {reset} from '../features/auth/authSlice'
 
 function Dashboard() {
   const navigate = useNavigate()
