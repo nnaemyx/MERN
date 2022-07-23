48007425c41f2905591b6876be0643426f123ad8
tezon <tezonteam@gmail.com>
2022-07-23 08:41:23 -0700

    new

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -46,7 +46,7 @@ function Dashboard() {
       <GoalForm />
 
       <section className='content'>
-        {goals.length > 0 ? (
+        {goals ?.length > 0 ? (
           <div className='goals'>
             {goals.map((goal) => (
               <GoalItem key={goal._id} goal={goal} />
